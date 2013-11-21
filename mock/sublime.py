eaa99a377a72cd31f1fb18a319bce2b21741aa2a
Ben Wiklund <ben@daisyowl.com>
2013-11-20 18:22:04 -0800

    setup up for unit tests

---
+++ mock/sublime.py
@@ -0,0 +1,1 @@
+def load_settings(*args): return
---
+++ spec.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+import cdnjs
+
+class CdnjsTests(unittest.TestCase):
+
+  def testOne(self):
+    self.failUnless( True )
+
+def main():
+  unittest.main()
+
+if __name__ == '__main__':
+  main()
